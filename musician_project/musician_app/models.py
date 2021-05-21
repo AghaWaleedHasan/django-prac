07e040bf10547ff839f7c719a627d576a6ad9763
waleed <waleed.hasan@datics.ai>
2021-05-21 10:33:21 +0500

    first commit

--- musician_project/musician_app/models.py
+++ musician_project/musician_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Musician(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=150)
+
+    def __str__(self):
+        return(self.name)
+
+class Album(models.Model):
+    musician = models.ForeignKey(Musician, on_delete=CASCADE, related_name='albums')
+    name = models.CharField(max_length=200)
+    release_year = models.CharField(max_length=4)
+
+    def __str__(self):
+        return(self.name)
+
+class Song(models.Model):
+    musician = models.ForeignKey(Musician, on_delete=CASCADE)
+    album = models.ForeignKey(Album, on_delete = CASCADE, related_name='album_songs')
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return(self.name)
